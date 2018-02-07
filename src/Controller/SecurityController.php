473ae56aefe106f63c5d75a2a5f68308e3c89ce2
David <ledavid64@gmail.com>
2018-02-07 14:54:46 -0800

    Reformat: I putted the "inscription" and "login" forms in same page
    Delete the RegistrationController now everything is in SecurityController

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -3,27 +3,75 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use App\Form\UserType;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/connexion", name="login")
+     * @Route("/inscription", name="signup")
      */
-    public function login(Request $request, AuthenticationUtils $authUtils): Response
+    public function login(Request $request, AuthenticationUtils $authUtils, UserPasswordEncoderInterface $passwordEncoder)
     {
+        // ##### Login Form #####
+
         // get the login error if there is one
         $error = $authUtils->getLastAuthenticationError();
 
         // last username entered by the user
         $lastUsername = $authUtils->getLastUsername();
 
+        // ##### Inscription Form #####
+
+        // 1) build the form
+        $user = new User();
+        $form = $this->createForm(UserType::class, $user,[
+            'action' => '/inscription'
+        ]);
+
+        // 2) handle the submit (will only happen on POST)
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            // 3) Encode the password (you could also do this via Doctrine listener)
+            $password = $passwordEncoder->encodePassword($user, $user->getPlainPassword());
+            $user->setPassword($password);
+            $user->setRoles(User::ROLE_USER);
+
+            // 4) save the User!
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+            // ... do any other work - like sending them an email, etc
+            // maybe set a "flash" success message for the user
+
+            return $this->redirectToRoute('homepage');
+        }
+
         return $this->render('Security/login.html.twig', [
+            'form' => $form->createView(),
             'last_username' => $lastUsername,
             'error' => $error,
         ]);
+    }
+
+    /**
+     * This is the route the user can use to logout.
+     *
+     * But, this will never be executed. Symfony will intercept this first
+     * and handle the logout automatically. See logout in config/packages/security.yaml
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout() : void
+    {
+        throw new \Exception('This should never be reached!');
     }
 }
